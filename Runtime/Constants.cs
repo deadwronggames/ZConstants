7fad80b2c7f2fdfc879731dd27f858e16e3f89e8
niabicht <nj_abicht@outlook.com>
2025-09-19 12:56:51 +0200

    cleanup

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -3,7 +3,6 @@ namespace DeadWrongGames.ZConstants
     public static class Constants
     {
         // File names
-        public const string PERSISTENT_GO_FILE_NAME_WITH_EXTENSION = "PF_PersistentGO.prefab";
         
         // Folder names
         public const string ROOT_FOLDER_NAME = "Assets";
@@ -14,7 +13,6 @@ namespace DeadWrongGames.ZConstants
         public const string SERVICES_SOUND_DATA_SO_FOLDER_NAME = "SoundDataSO";
         
         // Paths
-        public const string PACKAGE_PERSISTENT_GO_FULL_PATH = "Packages/ZServices/Runtime/_Assets/" + "/" + PERSISTENT_GO_FILE_NAME_WITH_EXTENSION;
         public const string SERVICE_RESOURCE_PATH = SERVICES_FOLDER_NAME + "/" + SERVICES_ASSETS_FOLDER_NAME + "/Resources";
         public const string SERVICE_RESOURCE_FULL_PATH = ROOT_FOLDER_NAME + "/" + PROJECT_FOLDER_NAME + "/" + SERVICE_RESOURCE_PATH;
     }
