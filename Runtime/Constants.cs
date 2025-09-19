d51c01ffdf531de0eac6ae6ebd78ec33de43155e
niabicht <nj_abicht@outlook.com>
2025-09-19 11:08:46 +0200

    crypto paths

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -11,6 +11,8 @@ namespace DeadWrongGames.ZConstants
         public const string SERVICES_SOUND_DATA_SO_FOLDER_NAME = "SoundDataSO";
         
         // Paths
-        public const string PERSISTENT_GO_PATH = "Packages/ZServices/Runtime/_Assets/PF_PersistentGO.prefab";
+        public const string PERSISTENT_GO_FULL_PATH = "Packages/ZServices/Runtime/_Assets/PF_PersistentGO.prefab";
+        public const string SERVICE_RESOURCE_PATH = SERVICES_FOLDER_NAME + "/" + SERVICES_ASSETS_FOLDER_NAME + "/Resources";
+        public const string SERVICE_RESOURCE_FULL_PATH = ROOT_FOLDER_NAME + "/" + PROJECT_FOLDER_NAME + "/" + SERVICE_RESOURCE_PATH;
     }
 }
