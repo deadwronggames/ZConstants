7ac817d06d73eab0a13e84791bf4a820a69482aa
niabicht <nj_abicht@outlook.com>
2025-09-19 09:57:50 +0200

    adding first constants

---
+++ Runtime/Constants.cs
@@ -0,0 +1,13 @@
+namespace DeadWrongGames.ZConstants
+{
+    public static class Constants
+    {
+        // Folder names
+        public const string ROOT_FOLDER_NAME = "Assets";
+        public const string PROJECT_FOLDER_NAME = "_Project";
+        public const string SERVICES_FOLDER_NAME = "Services";
+        public const string SERVICES_ASSETS_FOLDER_NAME = "Assets";
+        public const string SERVICES_EVENT_CHANNEL_SO_FOLDER_NAME = "EventChannelSO";
+        public const string SERVICES_SOUND_DATA_SO_FOLDER_NAME = "SoundDataSO";
+    }
+}
